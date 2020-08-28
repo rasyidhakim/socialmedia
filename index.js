861ca8283f8ae3906c5d48a6da5580346b16a161
Rasyid <rasyid.project@gmail.com>
2020-08-28 15:44:14 +0700

    [Rasyid] Added middleware for post endpoint

--- index.js
+++ index.js
@@ -46,10 +46,34 @@ app.get('/screams', async(req,res) => {
   }
 })
 
-app.post('/screams', async(req,res) => {
+const FBAuth = async (req,res,next) => {
+  let idToken;
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+    idToken = req.headers.authorization.split('Bearer ')[1]
+  } else {
+    console.error('No token found')
+    return res.status(403).json({ error: 'Unauthorized' })
+  }
+
+  try {
+    req.user = await admin.auth().verifyIdToken(idToken)
+    let data =  await db.collection('users')
+      .where('userId', '==', req.user.uid)
+      .limit(1)
+      .get()
+    req.user.handle = data.docs[0].data().handle
+    console.log(data.docs[0].data())
+    next()
+  } catch (e) {
+    console.error('Error while verifying token ',e)
+    return res.status(403).json(e)
+  }
+}
+
+app.post('/screams', FBAuth, async(req,res) => {
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   }
   try {
