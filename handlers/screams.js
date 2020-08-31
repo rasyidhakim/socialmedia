086183207e1cad037c5913387e1c07240361830d
Rasyid <rasyid.project@gmail.com>
2020-09-01 04:22:59 +0700

    [Rasyid] Added endpoint to like, unlike and delete post

--- handlers/screams.js
+++ handlers/screams.js
@@ -27,11 +27,16 @@ exports.postOneScream = async(req,res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.user.handle,
+    userImage: req.user.imageUrl,
+    likeCount: 0,
+    commentCount: 0,
     createdAt: new Date().toISOString()
   }
   try {
     let data = await db.collection('screams').add(newScream)
-    return res.json({ message: `document ${data.id} created successfully`})
+    const resScream = newScream
+    resScream.screamId = data.id
+    return res.json(resScream)
   } catch (e) {
     res.status(500).json({ error: 'something is wrong, i can feel it'})
     console.error(e)
@@ -78,10 +83,96 @@ exports.commentOnScream = async (req,res) => {
   try {
     const doc = await db.doc(`/screams/${req.params.screamId}`).get()
     if(!doc.exists) return res.status(404).json({ error: 'Scream not found' })
+    await doc.ref.update({ commentCount: doc.data().commentCount+1 })
     await db.collection('comments').add(newComment)
     res.json(newComment)
   } catch (e) {
     console.error(e)
     res.status(500).json({ error: 'Something\'s wrong, I can feel it' })
+  }
+}
+
+// Like a scream
+exports.likeScream = async (req,res) => {
+  try {
+    const likeDocument = db.collection('likes')
+      .where('userHandle','==',req.user.handle)
+      .where('screamId','==',req.params.screamId).limit(1)
+    
+    const screamDocument = db.doc(`/screams/${req.params.screamId}`)
+
+    let screamData = {}
+
+    let doc = await screamDocument.get()
+    if(doc.exists){
+      screamData = doc.data()
+      screamData.screamId = doc.id
+      let data = await likeDocument.get()
+
+      if(data.empty){
+        await db.collection('likes').add({
+          screamId: req.params.screamId,
+          userHandle: req.user.handle
+        })
+
+        screamData.likeCount++
+        await screamDocument.update({ likeCount: screamData.likeCount })
+        return res.json(screamData)
+      } else return res.status(400).json({ error: 'Scream already liked' })
+        
+    } else return res.status(404).json({ error: 'Scream not found' })
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ error: e.code})
+  }
+}
+
+// Unlike a scream
+exports.unlikeScream = async (req,res) => {
+  try {
+    const likeDocument = db.collection('likes')
+      .where('userHandle','==',req.user.handle)
+      .where('screamId','==',req.params.screamId).limit(1)
+    
+    const screamDocument = db.doc(`/screams/${req.params.screamId}`)
+
+    let screamData = {}
+
+    let doc = await screamDocument.get()
+    if(doc.exists){
+      screamData = doc.data()
+      screamData.screamId = doc.id
+      let data = await likeDocument.get()
+
+      if(data.empty) return res.status(400).json({ error: 'Scream not liked' })
+      else {
+        await db.doc(`/likes/${data.docs[0].id}`).delete()
+        screamData.likeCount--
+        await screamDocument.update({ likeCount: screamData.likeCount})
+        return res.json(screamData)
+      }
+        
+    } else return res.status(404).json({ error: 'Scream not found' })
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ error: e.code})
+  }
+}
+
+// Delete a scream
+exports.deleteScream = async (req,res) => {
+  try {
+    const document = db.doc(`/screams/${req.params.screamId}`)
+    const doc = await document.get()
+    console.log(req.params.screamId)
+    console.log(doc.data())
+    if(!doc.exists) return res.status(404).json({ error: 'Scream not found'})
+    if(doc.data().userHandle !== req.user.handle)
+      return res.status(403).json({ error: 'You can\'t delete someone else scream (Unauthorized)' })
+    await document.delete()
+    res.json({ message: `Scream deleted successfully` })
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ error: e.code})
   }
 }
--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const app = express()
 
 const FBAuth = require('./util/fbAuth')
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams')
+const { getAllScreams, postOneScream, getScream, commentOnScream, 
+  likeScream, unlikeScream,deleteScream } = require('./handlers/screams')
 const { signup,login, uploadImage, addUserDetails, getCurrentUser } = require('./handlers/users')
 
 // Screams Route
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream )
 app.get('/scream/:screamId', getScream)
-// TODO: delete scream
-// TODO: like a scream
-// TODO: unlike a scream
+app.delete('/scream/:screamId', FBAuth, deleteScream)
+app.get('/scream/:screamId/like', FBAuth, likeScream)
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream)
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
 
 // Users route
